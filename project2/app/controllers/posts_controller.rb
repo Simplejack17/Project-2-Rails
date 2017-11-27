c333bcc3a919d3787a7a3af218465294e928dd9f
Simplejack17 <jack.tyburski1@gmail.com>
2017-11-27 12:58:49 -0500

    got post index and show working. Also added seed data

--- project2/app/controllers/posts_controller.rb
+++ project2/app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    
+    params.require(:post).permit(:title, :content)
+
   end
 
 end
--- project2/db/seeds.rb
+++ project2/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Comment.destroy_all
+
+
+
+test_post1 = Post.create([{title: "Test Title 1", content: "Test Content1"}])
+test_post2 = Post.create([{title: "Test2", content: "test content2"}])
+
+
+Comment.create([{comment_content: "test", post_id: "test comment ID"}])
