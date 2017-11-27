ba6b2a7a2eb5362df43b28242ee4f84e520cdbcf
Simplejack17 <jack.tyburski1@gmail.com>
2017-11-27 12:30:06 -0500

    took out all USER stuff, going to implement it later. Also created database and migrated posts and comments.

--- project2/db/migrate/20171127145952_create_posts.rb
+++ project2/db/migrate/20171127145952_create_posts.rb
@@ -1,11 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :title         null: false, default: ""
-      t.string :content       null: false, default: ""
+      t.string :title
+      t.string :content
 
 
-      t.references :user, index: true, foreign_key: true
       t.timestamps
     end
   end
--- project2/db/migrate/20171127150047_create_comments.rb
+++ project2/db/migrate/20171127150047_create_comments.rb
@@ -2,11 +2,10 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.string :comment_content
-      t.string :comment_date
+
+
 
       t.references :post, index: true, foreign_key: true
-
-      t.references :user, index: true, foreign_key: true
       t.timestamps
     end
   end
