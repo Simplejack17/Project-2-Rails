b6475fd61efae0d60dabdfbb4c16da70a2c39e42
Simplejack17 <jack.tyburski1@gmail.com>
2017-11-28 21:26:00 -0500

    added user profile page

--- project2/config/routes.rb
+++ project2/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "posts#index"
+  devise_for :users
+  resources :users, only: [:show]
   resources :posts do
-      resources :comments
-    end
+    resources :comments
+  end
 end
